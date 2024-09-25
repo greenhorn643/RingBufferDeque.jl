30b9f43b4f99f29b69ed00fe150fba57f6818894
Alex Palmer <alex.palmer643@gmail.com>
2024-09-25 19:56:42 +1200

    Added RingBufferDeque implementation and tests

--- src/RingBufferDeque.jl
+++ src/RingBufferDeque.jl
@@ -1,5 +1,96 @@
 module RingBufferDeque
+export RBDeque
 
-# Write your package code here.
-
+mutable struct RBDeque{T} <: AbstractVector{T}
+    buf::Vector{T}
+    offset::Int
+    len::Int
+    empty_value::T
 end
+
+RBDeque{T}(; empty_value=zero(T)) where T =
+    RBDeque([empty_value], 0, 0, empty_value)
+
+Base.propertynames(::RBDeque{T}, private::Bool=false) where T =
+    private ? (:buf, :offset, :len, :empty_value, :cap, :len) : ()
+
+function Base.getproperty(d::RBDeque{T}, s::Symbol) where T
+    if s === :cap
+        return length(getfield(d, :buf))
+    else
+        return getfield(d, s)
+    end
+end
+
+Base.size(d::RBDeque{T}) where T = (d.len,)
+
+function Base.push!(d::RBDeque{T}, value::T) where T
+    if d.len == d.cap
+        increase_capacity!(d)
+    end
+
+    idx = ((d.offset + d.len) & (d.cap - 1)) + 1
+    d.buf[idx] = value
+    d.len += 1
+
+    return d
+end
+
+function Base.pop!(d::RBDeque{T})::T where T
+    if d.len < 1
+        throw(ArgumentError("deque must be non-empty"))
+    end
+
+    idx = (d.len + d.offset - 1) & (d.cap - 1) + 1
+    d.len -= 1
+    value = d.buf[idx]
+    d.buf[idx] = d.empty_value
+    return value
+end
+
+function Base.pushfirst!(d::RBDeque{T}, value::T) where T
+    if d.len == d.cap
+        increase_capacity!(d)
+    end
+
+    idx = (d.offset + d.cap - 1) & (d.cap - 1) + 1
+    d.buf[idx] = value
+    d.offset = idx - 1
+    d.len += 1
+
+    return d
+end
+
+function Base.popfirst!(d::RBDeque{T})::T where T
+    if d.len < 1
+        throw(ArgumentError("deque must be non-empty"))
+    end
+
+    idx = d.offset + 1
+    value = d.buf[idx]
+    d.buf[idx] = d.empty_value
+    d.len -= 1
+    d.offset = idx & (d.cap - 1)
+    return value
+end
+
+function increase_capacity!(d::RBDeque{T}) where T
+    old_cap = d.cap
+    append!(d.buf, d.empty_value for i in 1 : old_cap)
+    
+    n_items_to_move = d.offset - old_cap + d.len
+    v1 = @view d.buf[1 : n_items_to_move]
+    v2 = @view d.buf[old_cap + 1 : old_cap + n_items_to_move]
+
+    copy!(v2, v1)
+    fill!(v1, d.empty_value)
+
+    return nothing
+end
+
+function Base.getindex(d::RBDeque{T}, i::Int)::T where T
+    idx = (i + d.offset - 1) & (d.cap - 1) + 1
+    return d.buf[idx]
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,88 @@
 using RingBufferDeque
 using Test
 
+function lengthtest(n, m=1000)
+    d = RBDeque{Int}()
+
+    length_matches_expected = true
+
+    for i in 1:n
+        x = rand(-m:m)
+
+        f_or_b = rand(1:2)
+
+        if f_or_b == 1
+            pushfirst!(d, x)
+        else
+            push!(d, x)
+        end
+
+        if length(d) != i
+            length_matches_expected = false
+            break
+        end
+    end
+
+    @test length_matches_expected
+end
+
+function pushtest(n, m=1000)
+    d = RBDeque{Int}()
+    v = Vector{Int}()
+
+    for _ in 1:n
+        x = rand(-m:m)
+
+        f_or_b = rand(1:2)
+
+        if f_or_b == 1
+            pushfirst!(d, x)
+            pushfirst!(v, x)
+        else
+            push!(d, x)
+            push!(v, x)
+        end
+    end
+
+    @test collect(d) == v
+end
+
+function poptest(n, m=1000)
+    v = [rand(-m:m) for _ in 1 : n]
+    d = RBDeque{Int}()
+    for x in v
+        push!(d, x)
+    end
+
+    popped_v = zeros(Int, 0)
+    popped_d = zeros(Int, 0)
+
+    for _ in 1 : n
+        f_or_b = rand(1:2)
+
+        if f_or_b == 1
+            push!(popped_v, popfirst!(v))
+            push!(popped_d, popfirst!(d))
+        else
+            push!(popped_v, pop!(v))
+            push!(popped_d, pop!(d))
+        end
+    end
+
+    @test popped_v == popped_d && length(d) == 0
+end
+
 @testset "RingBufferDeque.jl" begin
-    # Write your tests here.
+    @test collect(RBDeque{Int}()) == []
+    @test_throws ArgumentError pop!(RBDeque{Int}())
+    @test_throws ArgumentError popfirst!(RBDeque{Int}())
+    lengthtest(10000)
+    pushtest(10)
+    pushtest(100)
+    pushtest(1000)
+    pushtest(10000)
+    poptest(10)
+    poptest(100)
+    poptest(1000)
+    poptest(10000)
 end
